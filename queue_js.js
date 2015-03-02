706d9bf203e2f4c976ad9397b7f8baf4dbde8965
Katherine Defliese <kdefliese@gmail.com>
2015-03-01 18:07:05 -0800

    Adding final files with animation

--- queue_js.js
+++ queue_js.js
@@ -7,23 +7,47 @@ var emailQueue = [];
 
 $(document).ready(function() {
   emailQueue.push($email1);
+  $email1.delay(500).queue(function(){
+      $(this).removeClass('hidden').addClass('email').dequeue();
+      });
   emailQueue.push($email2);
+  $email2.delay(1000).queue(function(){
+      $(this).removeClass('hidden').addClass('email').dequeue();
+      });
   emailQueue.push($email3);
+  $email3.delay(1500).queue(function(){
+      $(this).removeClass('hidden').addClass('email').dequeue();
+      });
   emailQueue.push($email4);
+  $email4.delay(2000).queue(function(){
+      $(this).removeClass('hidden').addClass('email').dequeue();
+      });
   emailQueue.push($email5);
+ $email5.delay(2500).queue(function(){
+      $(this).removeClass('hidden').addClass('email').dequeue();
+      });
   console.log(emailQueue);
 });
 
+
 var $response = $('#response');
+var $responsecontent = $('#responsecontent');
 console.log($response);
  $('#button').on('click',function() {
     if (emailQueue[0] != null) {
+      $response.addClass("visible").delay(800).queue(function(){
+      $(this).removeClass("visible").dequeue();
+      });
+      $responsecontent.addClass("visible").delay(800).queue(function(){
+      $(this).removeClass("visible").dequeue();
+      });
+      var firstEmail = emailQueue[0];
+      firstEmail.addClass('hidden');
       emailQueue.splice(0,1);
       console.log(emailQueue);
-      $response.removeClass('hidden').addClass('.visible');
     }
     else {
-     alert('All finished!');
+     alert('All finished! Do you think penguins drink beer?');
     }
   });
 
